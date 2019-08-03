2f96e3b5a44fb8dffd161e04514c5ce59e2b11b2
E.B <edenbenbow@yahoo.com>
2019-08-03 01:10:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ function placeOrder(cardNumber) {
     return (`Your total cost is ${total}, which will be charged to the card ${placeOrder}.`)
   }
 
-console.log ("Sorry, we don't have a credit card on file for you.")
+console.log ("Sorry, we don\'t have a credit card on file for you.")
 
 }
