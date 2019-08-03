16ba91fdcaf644ff82b4bc0b3b65fd1db7985aeb
E.B <edenbenbow@yahoo.com>
2019-08-03 01:35:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ for (var i = 0; i < cart.length; i++) {
 }
 
 function placeOrder(cardNumber) {
-    if (cardNumber) {cart = []
+    if (cardNumber) {cart = [];
     return (`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
   }
 return ("Sorry, we don\'t have a credit card on file for you.");
