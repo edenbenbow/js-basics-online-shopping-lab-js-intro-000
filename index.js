a175ab68c2e824cc6ba96b4b2f620312549fe31e
E.B <edenbenbow@yahoo.com>
2019-08-03 00:48:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,46 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(100*(Math.random()));
+  var itemInfo = {
+    itemName: item,
+    itemPrice: price
+  };
+  cart.push(itemInfo);
+ return (`${item} has been added to your cart.`);
 }
 
 function viewCart() {
-  // write your code here
+
+  if (cart.length === 0) { return 'Your shopping cart is empty.'}
+  if (cart.length === 1) { return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`}
+
+  var result = "In your cart, you have "
+  for(var i=0; i<cart.length; i++) {
+    if (i > 0) { result += ", " }
+    if (i === cart.length - 1) { result += "and " }
+    result += `${cart[i].itemName} at $${cart[i].itemPrice}`;
+  }
+  result += ".";
+  return result;
 }
 
 function total() {
-  // write your code here
+  var cartTotal = 0
+  for (var i = 0; i < cart.length; i++) {
+    cartTotal += cart[i].itemPrice;
+  }
+  return cartTotal
 }
 
 function removeFromCart(item) {
-  // write your code here
+for (var i = 0; i < cart.length; i++) {
+  if (cart[i].itemName(item)) {
+    cart.splice(i, 1);
+    return cart;
+  }
+}
+  return ("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
