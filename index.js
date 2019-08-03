6b29c138b8383e2b183905bb4fa17c3f22434dab
E.B <edenbenbow@yahoo.com>
2019-08-03 01:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,6 +57,5 @@ function placeOrder(cardNumber) {
     return (`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
   }
 return ("Sorry, we don\'t have a credit card on file for you.");
-}
 
 }
